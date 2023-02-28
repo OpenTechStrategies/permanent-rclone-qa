e9b973c6b88dbfc6a5c5f41b23a5932b3a43ac6d
mkrishna-perm <mkrishna@permanent.org>
2023-02-28 16:30:37 -0600

    Generating the test files
    This PR creates copies of the test file we have on disk to a git ignored folder. While
    copying, it is also changing the file name to include special characters. The code also
    takes care of created the folder and replacing its content as needed.
    
    To test
    Run PHP script as php testTree.php.
    A new folder named generatedFiles will be created.
    New files with filesnames containing special characters should be added to that folder.

--- testTree.php
+++ testTree.php
@@ -1,13 +1,36 @@
 <?php
-$inputCharacters = ['-','/','?','@','!','*','#','%','&','{','}','<','>',' ','$','!',':','+','`','|','=','123','\\','\''];
+$inputCharacters = ['-','/','?','@','!','*','#','%','&','{','}','<','>',' ','\$','!',':','+','`','|','=','123','\\','\''];
 $filenameFirst  = "new";
 $filenameSecond  = "file";
 $outputFilename =[];
-
-foreach ($inputCharacters as $inputCharacter) {
-   $addMiddleCharacter = $filenameFirst.$inputCharacter.$filenameSecond;
-   $addStartCharacter = $inputCharacter.$filenameFirst.$filenameSecond;
-   $addEndCharacter = $filenameFirst.$filenameSecond.$inputCharacter;
-   array_push($outputFilename,$addMiddleCharacter, $addStartCharacter, $addEndCharacter);
+$source = __DIR__ . "/testfile.txt";
+$destination = __DIR__ ."/generatedFiles";
+if (!is_dir($destination)) {
+    mkdir($destination, 0777, true);
 }
-print_r($outputFilename);
+foreach ($inputCharacters as $inputCharacter) {
+    $addMiddleCharacter = $filenameFirst.htmlspecialchars($inputCharacter).$filenameSecond;
+    $addStartCharacter = htmlspecialchars($inputCharacter).$filenameFirst.$filenameSecond;
+    $addEndCharacter = $filenameFirst.$filenameSecond.htmlspecialchars($inputCharacter);
+
+    try {
+        if(file_exists($destination . '/' . $addStartCharacter . '.txt')) {
+            unlink($destination . '/' . $addStartCharacter . '.txt');
+        }
+        copy($source, $destination . '/' . $addStartCharacter.'.txt');
+
+        if(file_exists($destination . '/' . $addMiddleCharacter . '.txt')) {
+            unlink($destination . '/' . $addMiddleCharacter . '.txt');
+        }
+        copy($source, $destination . '/' . $addMiddleCharacter.'.txt');
+
+        if(file_exists($destination . '/' . $addEndCharacter . '.txt')) {
+            unlink($destination . '/' . $addEndCharacter . '.txt');
+        }
+        copy($source, $destination . '/' . $addEndCharacter.'.txt');
+    }
+    catch(Exception $e){
+        throw new Exception('File cannot be copied! ',$e);
+    }
+    array_push($outputFilename,$addMiddleCharacter, $addStartCharacter, $addEndCharacter);
+}
